1af94a61d28cc9cc87bf5837b0fbb0819d643d56
robert-fiedor <robert.fiedor@kaplan.com>
2015-06-14 23:59:06 -0400

    first steps

---
+++ two/Chimp.py
@@ -0,0 +1,46 @@
+__author__ = 'Rob'
+
+import os, pygame
+from pygame.locals import *
+from pygame.compat import geterror
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(main_dir, 'aaa/data')
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join(data_dir, name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error:
+        print ('Cannot load image:', fullname)
+        raise SystemExit(str(geterror()))
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
+
+
+class Chimp(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)  # call Sprite intializer
+        self.image, self.rect = load_image('chimp.bmp', -1)
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.rect.topleft = 10, 10
+        self.move = 9
+        self.dizzy = 0
+
+    def update(self):
+        self._walk()
+
+    def _walk(self):
+        "move the monkey across the screen, and turn at the ends"
+        newpos = self.rect.move((self.move, 0))
+        if self.rect.left < self.area.left or \
+                        self.rect.right > self.area.right:
+            self.move = -self.move
+            newpos = self.rect.move((self.move, 0))
+        self.rect = newpos
+
---
+++ two/Main.py
@@ -0,0 +1,63 @@
+# Import Modules
+import os, pygame
+from pygame.locals import *
+from pygame.compat import geterror
+
+if not pygame.font: print ('Warning, fonts disabled')
+if not pygame.mixer: print ('Warning, sound disabled')
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+data_dir = os.path.join(main_dir, 'data')
+
+from Chimp import *
+
+
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((500, 500))
+    pygame.display.set_caption('Monkey Fever')
+
+    # Create The Backgound
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((250, 250, 250))
+
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+
+    # Prepare Game Objects
+    clock = pygame.time.Clock()
+    chimp = Chimp()
+    # fist = Fist()
+    allsprites = pygame.sprite.RenderPlain((chimp))
+
+
+    # Main Loop
+    going = True
+    while going:
+        clock.tick(6)
+
+        # Handle Input Events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                going = False
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                going = False
+
+        allsprites.update()
+
+        # Draw Everything
+        screen.blit(background, (0, 0))
+        allsprites.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
+
+# Game Over
+
+
+# this calls the 'main' function when this script is executed
+if __name__ == '__main__':
+    main()
